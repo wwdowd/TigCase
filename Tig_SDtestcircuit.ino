3ead77a87741b86d47e51bb2b2839691ca3c4994
wwdowd <35940808+wwdowd@users.noreply.github.com>
2018-04-04 14:27:32 -0700

    Add files via upload

--- Tig_SDtestcircuit.ino
+++ Tig_SDtestcircuit.ino
@@ -2,7 +2,7 @@
 
 
 #include <Wire.h> // built in library, for I2C communications
-//#include "RTClib_Tig.h" // Date and time functions using a DS3231 RTC connected via I2C
+#include "RTClib_Tig.h" // Date and time functions using a DS3231 RTC connected via I2C
 #include <SPI.h> //build in library; SD card communicates through SPI using library SdFat
 //#include <EEPROM.h> // WD; built in library, for reading the serial number stored in EEPROM
 #include <SdFat.h>
@@ -18,7 +18,7 @@
 
 /*  GLOBAL VARIABLES  */
 
-//RTC_DS3231 rtc; // Real-time clock
+RTC_DS3231 rtc; // Real-time clock
 SdFat sd; //SD card
 SdFile myFile; //file to manipulate/add data to on SD
 const int sdCS = 10; //CS is pin 10 for SD handling
@@ -27,8 +27,9 @@ const int sdCS = 10; //CS is pin 10 for SD handling
 //int SerialNum;
 //float DOmgLSet;
 //SoftwareSerial doProbe(9,8); // RX, TX lines on Uno board, Dissolved Oxygen probe
-//DateTime newtime; // used to track time in main loop
-//DateTime curr; //used as current time in loop
+DateTime newtime; // used to track time in main loop
+DateTime curr; //used as current time in loop
+DateTime tempTime; 
 
 double c = 15; //hold readings from thermocouple
 double phMeas = 7.77; //hold readings from pH probe, was pHVolt
@@ -37,9 +38,9 @@ const unsigned long tenSec = 10*1000; //keeping track of time
 unsigned long lastSampleTime = 0; // so always writes first value
 
 // Declare placeholder name for output files written to SD card
-//char filename[] = "YYYYMMDD_HHMM_00.csv";
+char filename[] = "YYYYMMDD_HHMM_00.csv";
 
-//uint32_t unixtimeArray[1]; // store unixtime values temporarily
+uint32_t unixtimeArray[1]; // store unixtime values temporarily
 
 //Adafruit_MAX31855 thermocouple(5, 6, 7);
 
@@ -156,6 +157,119 @@ void setup() {
 //// ----------------------  data collection & control LOOP-----------------------------------   ////
 void loop() {
   milli=millis();
+  curr = rtc.now();
+  writeToSD();
+  printTimeToSerial(curr);
+  Serial.print(" ");
   Serial.println(milli);
   delay(2000);
+  
+  c = c + 0.1; 
+  phMeas = phMeas + 0.01; 
+  doMeas = doMeas + 0.01;
+  //myFile.close();
 }
+
+static void printTimeToSerial (DateTime curr) {
+  Serial.print(curr.year(), DEC);
+  Serial.print('/');
+  if (curr.month() < 10) {
+    Serial.print(F("0"));
+    }
+  Serial.print(curr.month(), DEC);
+  Serial.print('/');
+  if (curr.day() < 10) {
+    Serial.print(F("0"));
+    }
+  Serial.print(curr.day(), DEC);
+  Serial.print('\t');
+  if (curr.hour() < 10){
+    Serial.print(F("0"));
+    }
+  Serial.print(curr.hour(), DEC);
+  Serial.print(':');
+  if (curr.minute() < 10) {
+    Serial.print("0");
+    }
+  Serial.print(curr.minute(), DEC);
+  Serial.print(':');
+  if (curr.second() < 10) {
+    Serial.print(F("0"));
+    }
+  Serial.print(curr.second(), DEC);
+}
+
+void writeToSD (void) {//removed 'void' from (), didn't work
+   
+  // Flash the green LED every 30 seconds to show data is being collected
+//  if (newtime.second() % PULSE_INTERVAL == 0) {
+//    digitalWrite(GREENLED,HIGH);
+//    delay(5);
+//    digitalWrite(GREENLED,LOW);
+//  }
+  // Reopen logfile. If opening fails, notify the user
+
+//TESTED if removing this loop allows write to SD, it doesn't
+  if (!myFile.isOpen()) {
+    if (!myFile.open(filename, O_RDWR | O_CREAT | O_AT_END)) {
+      Serial.println(F("couldn't find filename"));//digitalWrite(ERRLED, HIGH); // turn on error LED
+    }
+  }
+  
+  // Step through each element of the sample data arrays
+  // and write them to the SD card
+  //for (byte i = 1) { //byte i = 0; i < 1; i++) { //i = 01 was SAMPLES_PER_SECOND in LM code
+    // Write the unixtime
+    myFile.print(unixtimeArray[1], DEC); // POSIX time value
+    myFile.print(F(","));
+    printTimeToSD(myFile, unixtimeArray[1]); // human-readable date
+    myFile.print(F(","));
+    // Print thermocouple value. 
+    myFile.print(c); 
+    myFile.print(F(","));
+    // Print ph sensor value
+    myFile.print(phMeas); // 
+    myFile.print(F(","));
+    // Print DO reading
+    myFile.println(doMeas); // printlnmakes new line
+  //}
+    myFile.close(); // force the buffer to empty
+    DateTime t1 = DateTime(unixtimeArray[0]);
+    // If the seconds value is 30, update the file modified timestamp
+    if (t1.second() % 30 == 0){
+      myFile.timestamp(T_WRITE, t1.year(),t1.month(),t1.day(),t1.hour(),t1.minute(),t1.second());
+    }
+}
+
+void printTimeToSD (SdFile, DateTime tempTime) { // 'tempTime' is placeholder
+    // Write the date and time in a human-readable format
+    // to the file on the SD card. 
+    myFile.print(tempTime.year(), DEC);
+    myFile.print(F("-"));
+    if (tempTime.month() < 10) {
+      myFile.print("0");
+    }
+    myFile.print(tempTime.month(), DEC);
+    myFile.print(F("-"));
+    if (tempTime.day() < 10) {
+      myFile.print("0");
+    }
+    myFile.print(tempTime.day(), DEC);
+    myFile.print(F(" "));
+    if (tempTime.hour() < 10){
+      myFile.print("0");
+    }
+    myFile.print(tempTime.hour(), DEC);
+    myFile.print(F(":"));
+    if (tempTime.minute() < 10) {
+      myFile.print("0");
+    }
+    myFile.print(tempTime.minute(), DEC);
+    myFile.print(F(":"));
+    if (tempTime.second() < 10) {
+      myFile.print("0");
+    }
+    myFile.print(tempTime.second(), DEC);
+}
+
+
