cd27fe21bdf6928ab13f8a460e556f573861bf57
wwdowd <35940808+wwdowd@users.noreply.github.com>
2019-06-19 15:57:44 -0700

    Add files via upload

---
+++ Tig_EEPROM_serial.ino
@@ -0,0 +1,30 @@
+#include <EEPROM.h>
+//from http://www.arduino.cc/playground/Code/EEPROMWriteAnything
+#include <EEPROMAnything.h>
+
+
+// Enter serialNumber to upload here
+char serialNumber[] = "Tig07";
+
+int pos = 0; //start at first spot in EEPROM memory
+
+void setup()
+{
+    Serial.begin(57600);
+    
+    EEPROM_writeAnything(pos, serialNumber);
+}
+
+void loop()
+{
+  char *readvalue = EEPROM.get(0,serialNumber);
+  if (readvalue[0] == 'T') {
+    
+    Serial.print(F("Read serial number: "));
+    Serial.println(readvalue);
+  } else {
+    Serial.print(F("No valid serial number: "));
+    Serial.println(readvalue);
+    
+  }
+}
