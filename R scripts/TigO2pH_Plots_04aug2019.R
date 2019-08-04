77edc94accb85e9738d8ab620678555189fcb973
wwdowd <35940808+wwdowd@users.noreply.github.com>
2019-08-04 06:37:22 -0700

    Add files via upload

---
+++ R scripts/TigO2pH_Plots_04aug2019.R
@@ -0,0 +1,41 @@
+#create one directory per Arduino
+#
+
+setwd("D:/My_documents/WSU/Experiments/Tigriopus/R code/pH DO test") 
+#set working directory to the file that contains all the .csv files for that water source.
+
+filenames <- choose.files(default = "", caption = "Select 1 or more TigBathXX files",multi = TRUE)#file.choose()
+
+#LINE BELOW WILL EXTRACT ALL CSV FILES
+#filenames <- list.files(full.names=TRUE)
+All <- lapply(filenames,function(i){
+  read.csv(i,header=TRUE)
+})
+df <- do.call(rbind.data.frame, All)
+
+#sort input data by rows by unixtime, in case filenaming gets out of order
+df <-df[order(df$unixtime),]
+
+df$Date <-as.POSIXct(df$date_time)
+
+#install.packages("ggplot2")
+#install.packages("gridExtra")
+
+library(ggplot2)
+library(gridExtra)
+
+pH <- ggplot(data = df, aes(y = pH,x =Date))+
+  geom_point()+
+  theme_bw()+ 
+  ylab("pH")+
+  xlab("Date (MM DD)")+
+  ggtitle("pH")
+
+O2 <- ggplot(data = df, aes(y = O2_mgL,x =Date))+
+  geom_point()+
+  theme_bw()+ 
+  ylab("O2 mg/L")+
+  xlab("Date (MM DD)")+
+  ggtitle("Oxygen")
+
+grid.arrange(pH, O2, ncol=2, nrow =1)
